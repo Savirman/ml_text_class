eaad6dab4caafc7af7636ca7e41196a49f2a568f
savirman <savirm@yandex.ru>
2024-04-01 04:21:30 +0900

    Modify test/test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,10 @@
-pass
+import unittest
+import os
+
+class TestMainFileExistence(unittest.TestCase):
+
+    def test_main_file_existence(self):
+        self.assertTrue(os.path.exists("main.py"), "Файл main.py не найден")
+
+if __name__ == "__main__":
+    unittest.main()
